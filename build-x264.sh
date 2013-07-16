ab10dcae0e01499230db27db4fb411d2ea70dbef
Julien Carsique <jcarsique@nuxeo.com>
2013-07-16 15:59:32 +0200

    NXBT-588 - allow build on systems with yasm<1.2.0

--- build-x264.sh
+++ build-x264.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -e
 
-BRANCH="stable"
+# For yasm < 1.2.0
+BRANCH="b924133cabd125286488e16cfa71488ad4105d63~"
+YASM_VERSIONS=$(apt-cache madison yasm | grep 'Packages$' | cut -d '|' -f 2)
+for yasm_version in $YASM_VERSIONS; do
+  # yasm >= 1.2.0 found
+  dpkg --compare-versions "$yasm_version" ge "1.2.0" && BRANCH="stable" && break
+done
 
 if [ -d "x264" ]; then
     pushd x264
+    # Can't pull if is on a detached head 
+    git pull 2>/dev/null || git fetch --all
+    git checkout $BRANCH
     if [ -f "Makefile" ]; then
         make distclean || true
     fi
-    git pull
     popd
 else
     git clone git://git.videolan.org/x264
